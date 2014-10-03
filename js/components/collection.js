32d7ca5195c84f6bde0e210b506bafb806264847
guillaume <guillaume.chave@gmail.com>
2014-10-03 17:04:36 +0200

    add other way to return collection links (timestamp *not tested*, and firebase)

--- js/components/collection.js
+++ js/components/collection.js
@@ -21,6 +21,10 @@ Collection.prototype.resolvePagination = function(req, count) {
             limit = 10;
         }
 
+        var since    = req.query.since  != undefined ? req.query.since : null;
+        var before   = req.query.before != undefined ? req.query.before : null;
+        var until    = req.query.until  != undefined ? req.query.until : null;
+
 
         limit = Math.max(limit, 1);
         var maxPage = Math.max(Math.ceil(count / limit), 1);
@@ -31,7 +35,10 @@ Collection.prototype.resolvePagination = function(req, count) {
             limit:      limit,
             page:       page,
             count:      count,
-            lastPage:   Math.max(Math.ceil(count / limit), 1)
+            lastPage:   Math.max(Math.ceil(count / limit), 1),
+            since: since,
+            before: before,
+            until: until
         };
 
         resolve(pagination);
@@ -63,6 +70,163 @@ Collection.prototype.generateLinks = function(req, pagination, count) {
     return Promise.resolve(links);
 }
 
+Collection.prototype.generateTimestampLinks = function(req, items, dateExtractor, pagination) {
+
+    var components = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true);
+    delete components.search;
+    var queryObj = components.query;
+
+    var getLink = function(name, value) {
+
+        delete queryObj.since;
+        delete queryObj.before;
+        delete queryObj.until;
+
+        var newQueryObj = _.extend(queryObj, {limit: pagination.limit});
+
+        newQueryObj[name] = value;
+        return url.format(_.extend(components, {query: newQueryObj}));
+    }
+
+    var getLink2 = function(params) {
+
+        delete queryObj.since;
+        delete queryObj.before;
+        delete queryObj.until;
+
+
+        var newQueryObj = _.extend(queryObj, _.extend(params, {limit: pagination.limit}));
+
+        return url.format(_.extend(components, {query: newQueryObj}));
+    }
+
+    hasFirst = function() {
+        if (items.length == 0) {
+            return false;
+        }
+
+        return pagination.before || pagination.since ? true : false;
+    }
+
+
+    hasLast = function() {
+        return (items.length != 0) ? true : false;
+    };
+
+    hasPrevious = function() {
+        if (items.length == 0)
+            return false;
+        return pagination.before || pagination.since ? true : false;
+    };
+
+    hasNext = function() {
+        if (items.length == 0)
+            return false;
+        if (pagination.since === 0)
+            return false;
+        if (items.length < pagination.limit)
+            return false;
+        return true;
+    };
+
+    var links = {};
+
+    if (hasFirst()) {
+
+        var item = _.first(items);
+        var value = item[dateExtractor];
+
+        links.first = getLink('until', value);
+    }
+
+    if (hasLast()) {
+
+        links['last'] = getLink('since', '0');
+        if (items.length != 0) {
+
+
+            var item = _.last(items);
+            var value = item[dateExtractor];
+
+            links['last'] = getLink2({'since': 0, 'before': value});
+        }
+    }
+
+    if (hasPrevious()) {
+
+        var item = _.first(items);
+        var value = item[dateExtractor];
+
+        links['previous'] = getLink('since', value);
+    }
+
+    if (hasNext()) {
+
+        var item = _.last(items);
+        var value = item[dateExtractor];
+        links['next'] = getLink('before', value);
+    }
+    console.log(links);
+    return Promise.resolve(links);
+}
+
+
+Collection.prototype.generateFirebaseLinks = function(req, items, dateExtractor, pagination) {
+
+    var components = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl, true);
+    delete components.search;
+    var queryObj = components.query;
+
+    var getLink = function(name, value) {
+
+        delete queryObj.since;
+        delete queryObj.before;
+
+        var newQueryObj = _.extend(queryObj, {limit: pagination.limit});
+
+        newQueryObj[name] = value;
+        return url.format(_.extend(components, {query: newQueryObj}));
+    }
+
+    console.log(pagination);
+
+    hasPrevious = function() {
+        if (items.length == 0)
+            return false;
+        
+        return pagination.before != null || pagination.since != null ? true : false;
+    };
+
+    hasNext = function() {
+        if (items.length == 0)
+            return false;
+        if (pagination.since === 0)
+            return false;
+        if (items.length < pagination.limit)
+            return false;
+        return true;
+    };
+
+    var links = {};
+
+
+    if (hasPrevious()) {
+
+        var item = _.first(items);
+        var value = item[dateExtractor];
+
+        links['previous'] = getLink('since', value);
+    }
+
+    if (hasNext()) {
+
+        var item = _.last(items);
+        var value = item[dateExtractor];
+        links['next'] = getLink('before', value);
+    }
+    
+    return Promise.resolve(links);
+}
 
 Collection.prototype.returnCollection = function(req, res, dataPromise, countPromise) {
     var self = this;
@@ -82,4 +246,54 @@ Collection.prototype.returnCollection = function(req, res, dataPromise, countPro
                 console.log(e.stack);
                 res.status(400).json({error: e});
             });
+}
+
+Collection.prototype.returnCollectionTimestamp = function(req, res, dataPromise, countPromise, dateExtractor) {
+    var self = this;
+
+    return countPromise()
+            .then(function(count) {
+                return self.resolvePagination(req, count);
+            }).then(function(pagination) {
+
+        return dataPromise(pagination).then(function(items) {
+            return Promise.props({
+                count: pagination.count,
+                items: items,
+                links: self.generateTimestampLinks(req, items, dateExtractor, pagination)
+            })
+        })
+
+    }).then(function(result) {
+        return res.json(result);
+    }).catch(function(e) {
+        console.log(e.stack);
+        res.status(400).json({error: e});
+    });
+}
+
+
+
+Collection.prototype.returnCollectionFirebase= function(req, res, dataPromise, countPromise, dateExtractor) {
+    var self = this;
+
+    return countPromise()
+            .then(function(count) {
+                return self.resolvePagination(req, count);
+            }).then(function(pagination) {
+
+        return dataPromise(pagination).then(function(items) {
+            return Promise.props({
+                count: pagination.count,
+                items: items,
+                links: self.generateFirebaseLinks(req, items, dateExtractor, pagination)
+            })
+        })
+
+    }).then(function(result) {
+        return res.json(result);
+    }).catch(function(e) {
+        console.log(e.stack);
+        res.status(400).json({error: e});
+    });
 }
